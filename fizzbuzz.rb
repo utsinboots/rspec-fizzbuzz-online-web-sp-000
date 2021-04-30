db5fc7ecee31d12f0fb0b21755cf9f148094a287
Utshant Gurung <utshantgurung@gmail.com>
2021-04-30 08:03:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-  if (number % 3 == 0) && (number % 5 == 0)
-    "FizzBuzz"
-  elsif number % 3 == 0
+  if number % 3 == 0
     "Fizz"
   elsif number % 5 == 0
     "Buzz"
+  elsif (number % 3 == 0) && (number % 5 == 0)
+    "FizzBuzz"    
   end
 end
