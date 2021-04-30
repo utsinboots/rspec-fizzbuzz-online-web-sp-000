b5ca801e729cfadf5f304bbcf8a65b5e4d7dac65
Utshant Gurung <utshantgurung@gmail.com>
2021-04-30 07:57:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,10 @@ def fizzbuzz(number)
   if (number % 3 == 0) && (number % 5 == 0)
     "FizzBuzz"
   end
-  else if number % 3 == 0
+  elseif number % 3 == 0
     "Fizz"
   end
-  else if number % 5 == 0
+  elseif number % 5 == 0
     "Buzz"
   end
 end
