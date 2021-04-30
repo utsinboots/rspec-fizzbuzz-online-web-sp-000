638b4c8aa18dabc648ac3fbf69b0684f5f8bbe96
Utshant Gurung <utshantgurung@gmail.com>
2021-04-30 07:40:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,28 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  else if int % 3 != 0
+    "nil"
+  else
+    ""
+end
+
+fizzbuzz(3)
+
+=begin
+  (int)
+  if int % 3 == 0
+    "Fizz"
+  end
+  else if int % 5 == 0
+    "Buzz"
+  end
+end
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+=end
